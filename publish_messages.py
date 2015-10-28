944750a316026a90dc40160d2840ab120ec9168d
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2015-10-29 02:07:52 +0530

    generate fixtures file

--- publish_messages.py
+++ publish_messages.py
@@ -3,12 +3,9 @@ import json
 import fedmsg
 import time
 
-with open('fixtures3.json', 'r') as infile:
-    raw_messages = json.load(infile)
+with open('fixtures.json', 'r') as infile:
+    raw_messages= json.load(infile)
     for count,raw_message in enumerate(raw_messages):
         number = random.randint(1, 11)
         time.sleep(number)
-        fedmsg.publish(msg=raw_message['msg'],
-                       topic='autocloud.image.aborted')
-        print "%d published .... "%count
-
+        fedmsg.publish(msg=raw_message['msg'], topic=raw_message['topic'][raw_message['topic'].find('autocloud'):])
